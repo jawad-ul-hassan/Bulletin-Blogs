6e9e468ad4caccb9bd76cfb9452140b0f07255c9
jawad-ul-hassan <intelligentjawad@gmail.com>
2022-03-23 03:38:01 +0500

    home page with mobile responsiveness

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
+// Hamburger
+let menu = document.querySelector('#mobile-menu');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+});
+
+//Hero Slider --- Home
 var swiper = new Swiper('.mySwiper', {
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  speed: 1500,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  // speed: 1500,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
