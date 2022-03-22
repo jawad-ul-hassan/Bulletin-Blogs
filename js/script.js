d2f59c52bc89b60f0106d107f1b775916571337f
jawad-ul-hassan <intelligentjawad@gmail.com>
2022-03-23 02:39:43 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.mySwiper', {
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  speed: 1500,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
